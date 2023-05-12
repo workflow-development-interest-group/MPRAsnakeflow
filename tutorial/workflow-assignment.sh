f8a01ddb2f8164fbf2c741c7d659081ce26ae016
Andrew Blair <andrew.blair@ucsf.edu>
2023-05-12 06:05:20 +0000

    Add workflow-assignment.sh

---
+++ tutorial/workflow-assignment.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Download tutorial data 
+cd MPRAsnakeflow/assoc_basic/data
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/samples/GSM4237nnn/GSM4237954/suppl/GSM4237954_9MPRA_elements.fa.gz
+zcat GSM4237954_9MPRA_elements.fa.gz |awk '{ count+=1; if (count == 1) { print } else { print substr($1,1,171)}; if (count == 2) { count=0 } }' > design.fa
+prefetch SRR10800986
+fastq-dump --gzip --split-files SRR10800986
